3a13c7a94b91551c12a4b2a19ed04e658d2b3f92
Gopinathvengadesan <vgopinath173@gmail.com>
2024-09-14 09:03:02 +0530

    Add files via upload

---
+++ webpage/web.js
@@ -0,0 +1,13 @@
+var sidenav = document.querySelector('.side-navbar')
+      
+
+
+function shownavbar()
+{
+ sidenav.style.left="0"
+}
+function closenavber()
+{
+    sidenav.style.left="-60%"
+}
+      
